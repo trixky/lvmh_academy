344637307b7cea879ecb2583e3b9184fda4b0541
Tiffany Manolis <tiffany.mano@gmail.com>
2024-01-23 13:28:44 +0100

    basic register form

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/register.dart';
 import 'package:mobile/screen.dart';
 
 void main() {
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Screen(),
+      home: RegistrationPage(),
     );
   }
 }
